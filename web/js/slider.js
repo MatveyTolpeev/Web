1574cf3f350795c000d76355e24adb39842269bc
MatveyTolpeev <61470886+MatveyTolpeev@users.noreply.github.com>
2020-12-28 14:45:18 +0500

    Update slider.js

--- web/js/slider.js
+++ web/js/slider.js
@@ -41,31 +41,67 @@ var slider2 = $('#slider2');
 var block = $('.block');
 
 function left() {
-    slide(slider2, slider1);
+    if(isLeft == false)
+        return;
+    slideLeft(slider2, slider1);
     isLeft = false;
 }
 
 function right() {
-    slide(slider1, slider2);
+    if(isLeft == true)
+        return;
+    slideRight(slider1, slider2);
     isLeft = true;
 }
 
-function slide(slider1, slider2) {
+function slideRight(slider1, slider2) {
     slider2.css('left', '0%');
     slider1.css('left', '-50%');
     block.css('z-index', '6');
-    setTimeout(function () {
+    setTimeout(function ()
+    {
         slider1.css('transition', '0s');
         slider1.css('opacity', '0');
-        setTimeout(function () {
+        setTimeout(function ()
+        {
             slider1.css('left', '50%');
-            setTimeout(function () {
+            setTimeout(function ()
+            {
                 slider1.css('opacity', '1');
                 slider1.css('transition', '1s');
                 block.css('z-index', '4');
             }, 20);
         }, 10);
     }, 600);
+}
+
+function slideLeft(slider1, slider2) {
+    slider2.css('left', '0%');
+    slider1.css('left', '-50%');
+    block.css('z-index', '6');
+    setTimeout(function ()
+    {
+        slider1.css('transition', '0s');
+        slider1.css('opacity', '0');
+        setTimeout(function ()
+        {
+            slider1.css('left', '50%');
+            setTimeout(function ()
+            {
+                slider1.css('opacity', '1');
+                slider1.css('transition', '1s');
+                block.css('z-index', '4');
+            }, 20);
+        }, 10);
+    }, 600);
+}
+
+document.getElementById('slider').onmouseover = function(){
+    clearInterval(30000);
+}
+
+document.getElementById('slider').onmouseout = function(){
+    clearInterval(5000);
 }
 
 
